888fc1e53fe2d5d8a791a33360306e02d44d7b90
Raj <117701775+Acronius26@users.noreply.github.com>
2023-04-20 01:14:57 -0400

    Multani, Raj, 4/19/2023: Updated locations.php with correct data and to utilize in locations.js code to function for locations.html

---
+++ php/locations.php
@@ -0,0 +1,9 @@
+[{"id":"","name":"Berlin","iso":"DE","latitude":"52.52437","longitude":"13.41053","population":"3426354","elevation":"43"},
+{"id":"","name":"London","iso":"GB","latitude":"51.50853","longitude":"-0.12574","population":"7556900","elevation":"25"},
+{"id":"","name":"Barcelona","iso":"ES","latitude":"41.3874","longitude":"2.1686","population":"1664182","elevation":"39"},
+{"id":"","name":"Paris","iso":"FR","latitude":"48.85341","longitude":"2.3488","population":"2138551","elevation":"42"},
+{"id":"","name":"Toronto","iso":"CA","latitude":"43.6532260","longitude":"-79.3831843","population":"4612191","elevation":"175"},
+{"id":"","name":"Mexico City","iso":"MX","latitude":"19.4326","longitude":"-99.1332","population":"9209944","elevation":"7350"},
+{"id":"","name":"Detroit","iso":"US","latitude":"42.3314270","longitude":"-83.0457538","population":"677116","elevation":"656"},
+{"id":"","name":"Louisville","iso":"US","latitude":"38.2527","longitude":"-85.7585","population":"766757","elevation":"466"},
+{"id":"","name":"Burlington","iso":"US","latitude":"44.4758825","longitude":"-73.2120720","population":"42452","elevation":"200"}]
